787a273ee87c4ac9f3c804a301b8535914bfab35
RachitKumar <44887572+kumarsiddappa-git@users.noreply.github.com>
2025-04-20 08:37:29 +0530

    Update mytext-1.cpp

--- mytext-1.cpp
+++ mytext-1.cpp
@@ -6,37 +6,37 @@
 
 int main(void) {
     
-	void * exec_mem;
+	void * exec_memory;
 	BOOL rv;
 	HANDLE th;
     DWORD oldprotect = 0;
 
-	// 4 byte payload
+	// 4 byte payload since we are runing on 64bit
 	unsigned char payload[] = {
 		0x40,		// NOP
 		0x90,		// NOP
 		0xcc,		// INT3
 		0xc3		// RET
 	};
-	unsigned int payload_len = 4;
+	unsigned int payload_len = 4;   // hard coded , we can use the sizeof function to get the length
 	
 	// Allocate a memory buffer for payload mentioned above
-	exec_mem = VirtualAlloc(0, payload_len, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	exec_memory = VirtualAlloc(0, payload_len, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 	printf("%-20s : 0x%-016p\n", "payload addr", (void *)payload);
 	printf("%-20s : 0x%-016p\n", "exec_mem addr", (void *)exec_mem);
 
 	// Copy payload to new buffer that is copy payload to exec_mem and lenght is payload_len
-	RtlMoveMemory(exec_mem, payload, payload_len);
+	RtlMoveMemory(exec_memory, payload, payload_len);
 	
 	// Make new buffer as executable so we can execute the code 
-	rv = VirtualProtect(exec_mem, payload_len, PAGE_EXECUTE_READ, &oldprotect);
+	rv = VirtualProtect(exec_memory, payload_len, PAGE_EXECUTE_READ, &oldprotect);
 
 	printf("\nHit me!\n");
 	getchar(); // wait as interrupt to check how the code is saved 
 
 	// If all good, run the payload by creating a thread in the current process
 	if ( rv != 0 ) {
-			th = CreateThread(0, 0, (LPTHREAD_START_ROUTINE) exec_mem, 0, 0, 0);
+			th = CreateThread(0, 0, (LPTHREAD_START_ROUTINE) exec_memory, 0, 0, 0);
 			WaitForSingleObject(th, -1);
 	}
 
